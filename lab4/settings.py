e5937c66b0c1968d2520acb938bdfc659b949da3
Ainur <ainur@example.com>
2026-04-27 23:58:38 +0500

    fix static files

--- lab4/settings.py
+++ lab4/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.middleware.WhiteNoiseMiddleware',
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
